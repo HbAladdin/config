04a5edae36036bea9c8371eafe0c076c378eb403
zhouruizhong <528441592@qq.com>
2019-01-01 15:27:14 +0800

    init commit

---
+++ src/main/java/com/zrz/config/ConfigApplication.java
@@ -0,0 +1,23 @@
+package com.zrz.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ *  分布式配置中心
+ * @author 周瑞忠
+ * @date 2019/1/1 14:56
+ */
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
